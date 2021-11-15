dbf53ebc35894bcf30aba33b8e8426ae42bb21cd
safoueneraddaoui <raddaoui.safouene.tkd@gmail.com>
2021-11-15 23:15:35 +0100

    delete client rest api

--- src/main/java/iit/tn/springboot_backend/controller/ClientController.java
+++ src/main/java/iit/tn/springboot_backend/controller/ClientController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -35,6 +37,32 @@ public class ClientController {
         Client client = clientDAO.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Client not exist with id :"+ id));
         return ResponseEntity.ok(client);
+    }
+
+    // update client rest api
+    @PutMapping("/clients/{id}")
+    public ResponseEntity <Client> updateClient(@PathVariable Long id,@RequestBody Client clientDetails){
+        Client client = clientDAO.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not exist with id :"+ id));
+        client.setCin(clientDetails.getCin());
+        client.setFirstName(clientDetails.getFirstName());
+        client.setLastName(clientDetails.getLastName());
+        client.setEmail(clientDetails.getEmail());
+
+        Client updatedClient = clientDAO.save(client);
+        return ResponseEntity.ok(client);
+    }
+
+    // delete client rest api
+    @DeleteMapping("/clients/{id}")
+    public ResponseEntity <Map<String, Boolean>> deleteClient(@PathVariable Long id){
+        Client client = clientDAO.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not exist with id :"+ id));
+
+        clientDAO.delete(client);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 
 }
