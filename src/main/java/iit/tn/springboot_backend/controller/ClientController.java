9d81be13a7ac9aa185d3528996bf77f3ec5c1a78
safoueneraddaoui <raddaoui.safouene.tkd@gmail.com>
2021-11-15 22:20:42 +0100

    Add client restapi

--- src/main/java/iit/tn/springboot_backend/controller/ClientController.java
+++ src/main/java/iit/tn/springboot_backend/controller/ClientController.java
@@ -3,10 +3,7 @@ package iit.tn.springboot_backend.controller;
 import iit.tn.springboot_backend.entity.Client;
 import iit.tn.springboot_backend.repository.ClientDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,11 @@ public class ClientController {
     @GetMapping("/clients")
     public List<Client> getAllClients(){
         return clientDAO.findAll();
+    }
+
+    // create client rest api
+    @PostMapping("/clients")
+    public Client createClient(@RequestBody Client client){
+        return clientDAO.save(client);
     }
 }
