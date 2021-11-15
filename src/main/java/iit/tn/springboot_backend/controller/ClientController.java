4d243e62c549be3b975bbd813e8cf2e0c57e03ea
safoueneraddaoui <raddaoui.safouene.tkd@gmail.com>
2021-11-15 22:46:07 +0100

    getClientById restapi

--- src/main/java/iit/tn/springboot_backend/controller/ClientController.java
+++ src/main/java/iit/tn/springboot_backend/controller/ClientController.java
@@ -1,8 +1,10 @@
 package iit.tn.springboot_backend.controller;
 
 import iit.tn.springboot_backend.entity.Client;
+import iit.tn.springboot_backend.exception.ResourceNotFoundException;
 import iit.tn.springboot_backend.repository.ClientDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,4 +28,13 @@ public class ClientController {
     public Client createClient(@RequestBody Client client){
         return clientDAO.save(client);
     }
+
+    // get client by id rest api
+    @GetMapping("/clients/{id}")
+    public ResponseEntity <Client> getClientById(@PathVariable Long id){
+        Client client = clientDAO.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Client not exist with id :"+ id));
+        return ResponseEntity.ok(client);
+    }
+
 }
